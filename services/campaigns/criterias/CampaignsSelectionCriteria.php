ccea97e7d4c3602d416e93fd1e3b9074624f0c2a
tugrik <tugriky@gmail.com>
2016-04-27 19:10:30 +0300

    up

--- services/campaigns/criterias/CampaignsSelectionCriteria.php
+++ services/campaigns/criterias/CampaignsSelectionCriteria.php
@@ -7,31 +7,13 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class CampaignsSelectionCriteria extends Model
 {
-    /**
-     * @var int[]
-     * @Assert\Count(
-     *     max=1000
-     * )
-     */
     public $Ids;
 
-    /**
-     * @var \directapi\services\campaigns\enum\CampaignTypeEnum[]
-     */
     public $Types;
 
-    /**
-     * @var \directapi\services\campaigns\enum\CampaignStateEnum[]
-     */
     public $States;
 
-    /**
-     * @var \directapi\services\campaigns\enum\CampaignStatusEnum[]
-     */
     public $Statuses;
 
-    /**
-     * @var \directapi\services\campaigns\enum\CampaignStatusPaymentEnum[]
-     */
     public $StatusesPayment;
 }
